8a91baf8c4e6a7bcb39a9dc91b9d59d82dceba90
matheus-1618 <matheus1207live@gmail.com>
2023-09-27 00:36:57 -0300

    Player movimenting

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
-    private Rigidbody2D rb;
     public float maxSpeed = 5;
+    public Transform groundCheck;
+    public float jumpForce;
+
+    private Rigidbody2D rb;
     private float speed;
     private bool facingRight = true;
+    private bool onGround;
+    private bool jump = false;
+    private bool doubleJump;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -19,17 +25,39 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        onGround = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+        if (onGround)
+        {
+            doubleJump = false;
+        }
+
+
+        if (Input.GetButtonDown("Jump") && (onGround || !doubleJump))
+        {
+            jump = true;
+            if (!doubleJump && !onGround)
+            {
+                doubleJump = true;
+            }
+        }
     }
 
     private void FixedUpdate()
     {
         float h = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(h * speed, rb.velocity.y);
+;
 
         if ((h > 0 && !facingRight) || (h < 0 && facingRight))
         {
             Flip();
+        }
+
+        if (jump)
+        {
+            rb.velocity = Vector2.zero;
+            rb.AddForce(Vector2.up * jumpForce);
+            jump = false;
         }
     }
 
